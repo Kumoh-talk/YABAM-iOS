e9c6b9d6ea8e0d73ff1e088e4406211a88b42204
박효준 <gywns626@naver.com>
2025-04-08 22:25:41 +0900

    design: Tuist로 만들어진 색상에 대한 네임스페이스 추가

---
+++ YABAM/Feature/Sources/Extensions/Color+YBColor.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+extension Color {
+    enum Neutral {
+        static let neutral900 = Color(asset: FeatureAsset.Colors.neutral900)
+        static let neutral800 = Color(asset: FeatureAsset.Colors.neutral800)
+        static let neutral700 = Color(asset: FeatureAsset.Colors.neutral700)
+        static let neutral600 = Color(asset: FeatureAsset.Colors.neutral600)
+        static let neutral500 = Color(asset: FeatureAsset.Colors.neutral500)
+        static let neutral400 = Color(asset: FeatureAsset.Colors.neutral400)
+        static let neutral300 = Color(asset: FeatureAsset.Colors.neutral300)
+        static let neutral200 = Color(asset: FeatureAsset.Colors.neutral200)
+        static let neutral100 = Color(asset: FeatureAsset.Colors.neutral100)
+        static let neutral50 = Color(asset: FeatureAsset.Colors.neutral50)
+    }
+}
