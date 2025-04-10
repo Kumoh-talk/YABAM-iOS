dcfb7a98e0ce04e02b696cf32a3035cfd110bf2e
박효준 <gywns626@naver.com>
2025-04-10 13:47:45 +0900

    feat: QR 인식 실패 시 에러문구 추가

--- YABAM/Feature/Sources/OrderView/OrderView.swift
+++ YABAM/Feature/Sources/OrderView/OrderView.swift
@@ -2,6 +2,9 @@ import SwiftUI
 import Core
 
 struct OrderView: View {
+    @State private var scannerError: String? = nil
+    @State private var isLoading: Bool = false
+    
     var body: some View {
         VStack(spacing: 0) {
             YBText(
@@ -10,7 +13,7 @@ struct OrderView: View {
                 color: .Neutral.neutral800
             )
             .multilineTextAlignment(.center)
-            .padding(.top, 100)
+            .padding(.top, 80)
             .lineLimit(1)
             .minimumScaleFactor(0.5)
             .padding(.horizontal, 24)
@@ -23,13 +26,35 @@ struct OrderView: View {
             .multilineTextAlignment(.center)
             
             YBCodeScanner { scannedString in
+                isLoading = true
                 if let url = URL(string: scannedString) {
-                    UIApplication.shared.open(url)
+                    // TODO: 모든 QR이 스캔되므로 나중에 도메인 나오면 야밤 QR만 인식하도록 변경하기
+                    /// https://github.com/Kumoh-talk/YABAM-iOS/pull/8#discussion_r2036429092
+                    UIApplication.shared.open(url) { success in
+                        isLoading = false
+                        if !success {
+                            YBLogger.error("QR 코드 스캔 실패: \(scannedString)")
+                            scannerError = "URL을 인식할 수 없습니다."
+                        }
+                    }
                 } else {
-                    YBLogger.error("Invalid URL from scanned string.")
+                    YBLogger.error("QR 코드 스캔 실패: \(scannedString)")
+                    isLoading = false
+                    scannerError = "QR 코드가 유효하지 않습니다, 다시 시도해주세요."
                 }
             }
-            .padding(.top, 50)
+            .padding(.top, 30)
+            
+            if let error = scannerError {
+                Text(error)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            
+            if isLoading {
+                ProgressView()
+                    .padding()
+            }
             
             Spacer()
         }
