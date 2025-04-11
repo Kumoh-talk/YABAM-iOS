d20ba7546e15bc665239517057d7e3cbb16bfded
박효준 <gywns626@naver.com>
2025-04-11 23:54:38 +0900

    feat: 모델에 isRecommended를 추가하여 주인장 추천인지 아닌지 구분하도록 설정

--- YABAM/Feature/Sources/Models/OrderMenu/MenuSection.swift
+++ YABAM/Feature/Sources/Models/OrderMenu/MenuSection.swift
@@ -12,6 +12,7 @@ struct MenuItem: Identifiable {
     let description: String
     let price: Int
     let imageName: String
+    let isRecommended: Bool
 }
 
 struct SampleData {
@@ -20,41 +21,37 @@ struct SampleData {
             id: "추천",
             title: "추천 메뉴",
             items: [
-                MenuItem(name: "우삼겹 야미볶음밥", description: "우삼겹과 볶음밥의 만남", price: 9500, imageName: "menu1"),
-                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2"),
-                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2"),
-                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2"),
-                MenuItem(name: "우삼겹 야미볶음밥", description: "우삼겹과 볶음밥의 만남", price: 9500, imageName: "menu1"),
-                MenuItem(name: "우삼겹 야미볶음밥", description: "우삼겹과 볶음밥의 만남", price: 9500, imageName: "menu1"),
-                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2"),
-                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2"),
-                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2"),
-                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2"),
-                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2"),
+                MenuItem(name: "우삼겹 야미볶음밥", description: "우삼겹과 볶음밥의 만남", price: 9500, imageName: "menu1", isRecommended: true),
+                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2", isRecommended: false),
+                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2", isRecommended: true),
+                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2", isRecommended: false),
+                MenuItem(name: "우삼겹 야미볶음밥", description: "우삼겹과 볶음밥의 만남", price: 9500, imageName: "menu1", isRecommended: true),
+                MenuItem(name: "우삼겹 야미볶음밥", description: "우삼겹과 볶음밥의 만남", price: 9500, imageName: "menu1", isRecommended: false),
+                MenuItem(name: "대패삼겹 야미볶음밥", description: "대패삼겹과 특제소스", price: 9500, imageName: "menu2", isRecommended: true),
             ]
         ),
         MenuSection(
             id: "한식",
             title: "한식ㄱㄱㄱㄱㄱㄱㄱ",
             items: [
-                MenuItem(name: "우삼겹 김치볶음밥", description: "우삼겹 + 김치 조합", price: 9500, imageName: "menu3"),
-                MenuItem(name: "대패삼겹 김치볶음밥", description: "대패삼겹 + 김치", price: 9500, imageName: "menu4")
+                MenuItem(name: "우삼겹 김치볶음밥", description: "우삼겹 + 김치 조합", price: 9500, imageName: "menu3", isRecommended: false),
+                MenuItem(name: "대패삼겹 김치볶음밥", description: "대패삼겹 + 김치", price: 9500, imageName: "menu4", isRecommended: true)
             ]
         ),
         MenuSection(
             id: "일식",
             title: "일식ㄷㄷㄷㄷㄷㄷ",
             items: [
-                MenuItem(name: "우삼겹 김치볶음밥", description: "우삼겹 + 김치 조합", price: 9500, imageName: "menu3"),
-                MenuItem(name: "대패삼겹 김치볶음밥", description: "대패삼겹 + 김치", price: 9500, imageName: "menu4")
+                MenuItem(name: "우삼겹 김치볶음밥", description: "우삼겹 + 김치 조합", price: 9500, imageName: "menu3", isRecommended: true),
+                MenuItem(name: "대패삼겹 김치볶음밥", description: "대패삼겹 + 김치", price: 9500, imageName: "menu4", isRecommended: false)
             ]
         ),
         MenuSection(
             id: "양식",
             title: "양식ㄴㄴㄴㄴㄴㄴㄴ",
             items: [
-                MenuItem(name: "우삼겹 김치볶음밥", description: "우삼겹 + 김치 조합", price: 9500, imageName: "menu3"),
-                MenuItem(name: "대패삼겹 김치볶음밥", description: "대패삼겹 + 김치", price: 9500, imageName: "menu4")
+                MenuItem(name: "우삼겹 김치볶음밥", description: "우삼겹 + 김치 조합", price: 9500, imageName: "menu3", isRecommended: false),
+                MenuItem(name: "대패삼겹 김치볶음밥", description: "대패삼겹 + 김치", price: 9500, imageName: "menu4", isRecommended: true)
             ]
         )
     ]
--- YABAM/Feature/Sources/Views/Order/OrderMenu/Section/MenuItemView.swift
+++ YABAM/Feature/Sources/Views/Order/OrderMenu/Section/MenuItemView.swift
@@ -11,10 +11,13 @@ struct MenuItemView: View {
                 .cornerRadius(8)
             
             VStack(alignment: .leading, spacing: 2) {
-                YBText("주인장 추천!", fontType: .boldBody3, color: .Neutral.neutral700)
-                    .padding(4)
-                    .background(Color.Neutral.neutral100)
-                    .cornerRadius(4)
+                if item.isRecommended {
+                    YBText("주인장 추천!", fontType: .boldBody3, color: .Neutral.neutral700)
+                        .padding(4)
+                        .background(Color.Neutral.neutral100)
+                        .cornerRadius(4)
+                }
+                
                 YBText(item.name, fontType: .boldHeader3, color: .Neutral.neutral900)
                 YBText(item.description, fontType: .mediumBody2, color: .Neutral.neutral600)
                 YBText("\(item.price)원", fontType: .mediumBody1, color: .Neutral.neutral900)
