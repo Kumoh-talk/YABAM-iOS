a966a7ca9e5ae6e287bf5b2cd9002d9ba99949bc
Kyxxn <gywns626@naver.com>
2025-04-26 12:29:57 +0900

    refactor: 임시 장바구니를 없애고, 하나의 장바구니로 동작하도록 로직 변경

--- YABAM/Feature/Sources/Views/Order/TableOrder/MenuOrder/MenuOrderView.swift
+++ YABAM/Feature/Sources/Views/Order/TableOrder/MenuOrder/MenuOrderView.swift
@@ -5,8 +5,6 @@ struct MenuOrderView: View {
     let sections: [MenuSection]
     @State private var selectedSectionID: String?
     @State private var isExitAlert = false
-    @State private var isNavigatingToTemporaryCart = false
-    @StateObject private var temporaryCart = CartManager()
     @ObservedObject var cartManager: CartManager
     @Environment(\.dismiss) private var dismiss
     
@@ -17,11 +15,12 @@ struct MenuOrderView: View {
                     sections: sections,
                     selectedSectionID: $selectedSectionID
                 )
+                .padding(.top, 2)
                 
                 MenuSectionList(
                     sections: sections,
                     selectedSectionID: $selectedSectionID,
-                    cartManager: temporaryCart
+                    cartManager: cartManager
                 )
                 .frame(maxHeight: .infinity)
             }
@@ -29,39 +28,11 @@ struct MenuOrderView: View {
         .navigationTitle("메뉴 주문하기")
         .navigationBarBackButtonHidden()
         .navigationBarTitleDisplayMode(.inline)
-        .task {
-            if temporaryCart.items.isEmpty {
-                PopGestureManager.shared.updatePopGestureState(isEnabled: true)
-            } else {
-                PopGestureManager.shared.updatePopGestureState(isEnabled: false)
-            }
-        }
-        .onDisappear {
-            PopGestureManager.shared.updatePopGestureState(isEnabled: true)
-        }
         .withNavigationButtons(
             leading: NavigationButtonConfig {
-                Image(.close)
+                Image(.popArrow)
             } action: {
-                if temporaryCart.hasItems {
-                    isExitAlert = true
-                } else {
-                    dismiss()
-                }
-            },
-            trailing: NavigationButtonConfig {
-                ZStack(alignment: .topTrailing) {
-                    Image(.basket)
-                    
-                    if temporaryCart.hasItems {
-                        Circle()
-                            .fill(Color.red)
-                            .frame(width: 6, height: 6)
-                            .offset(x: 2, y: -2)
-                    }
-                }
-            } action: {
-                isNavigatingToTemporaryCart = true
+                dismiss()
             }
         )
         .alert("메뉴가 저장되지 않고 사라집니다. 나가시겠어요?", isPresented: $isExitAlert) {
@@ -74,9 +45,6 @@ struct MenuOrderView: View {
             if selectedSectionID == nil {
                 selectedSectionID = sections.first?.id
             }
-        }
-        .navigationDestination(isPresented: $isNavigatingToTemporaryCart) {
-            MenuCartView(isTemporary: true, cartManager: temporaryCart)
         }
     }
 }
