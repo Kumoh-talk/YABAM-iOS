91420956984830cc53c033727b2c4a91b199978c
Kyxxn <gywns626@naver.com>
2025-04-28 12:14:25 +0900

    feat: 메뉴 주문하기에서 뒤로가기 제스쳐 방지

--- YABAM/Feature/Sources/Views/Order/TableOrder/MenuOrder/MenuOrderView.swift
+++ YABAM/Feature/Sources/Views/Order/TableOrder/MenuOrder/MenuOrderView.swift
@@ -29,6 +29,7 @@ struct MenuOrderView: View {
         .navigationTitle("메뉴 주문하기")
         .navigationBarBackButtonHidden()
         .navigationBarTitleDisplayMode(.inline)
+        .popGestureDisabled()
         .withNavigationButtons(
             leading: NavigationButtonConfig {
                 Image(.close)
