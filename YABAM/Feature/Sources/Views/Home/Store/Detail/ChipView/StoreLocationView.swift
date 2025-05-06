4beab7750a884b7927e25d613fbcad6470b12142
박효준 <gywns626@naver.com>
2025-05-06 23:51:35 +0900

    feat: 가게 상세보기에서 위치보기 칩 UI 구현 - MapKit을 통해 지도에 가게위치 표시

--- YABAM/Feature/Sources/Views/Home/Store/Detail/ChipView/StoreLocationView.swift
+++ YABAM/Feature/Sources/Views/Home/Store/Detail/ChipView/StoreLocationView.swift
@@ -1,12 +1,42 @@
 import SwiftUI
+import MapKit
 
 struct StoreLocationView: View {
     let store: Store
 
+    @State private var region: MKCoordinateRegion
+
+    init(store: Store) {
+        self.store = store
+        _region = State(initialValue: MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: store.latitude, longitude: store.longitude),
+            span: MKCoordinateSpan(latitudeDelta: 0.003, longitudeDelta: 0.003)
+        ))
+    }
+
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            YBText("위치: \(store.location)", fontType: .regularBody2, color: .Neutral.neutral600)
-            // TODO: 지도 SDK (예: Naver Map SDK) 또는 WebView 연동
+        VStack(alignment: .leading, spacing: 12) {
+            YBText("가게 위치", fontType: .boldBody1, color: .Neutral.neutral900)
+
+            Map(coordinateRegion: $region, annotationItems: [store]) { store in
+                MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: store.latitude, longitude: store.longitude)) {
+                    VStack(spacing: 4) {
+                        Image(.yabamFillLogo)
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                            .shadow(radius: 3)
+
+                        Image(systemName: "arrowtriangle.down.fill")
+                            .font(.caption)
+                            .foregroundColor(.blue)
+                            .offset(y: -4)
+                    }
+                }
+            }
+            .frame(height: 240)
+            .cornerRadius(12)
         }
     }
 }
