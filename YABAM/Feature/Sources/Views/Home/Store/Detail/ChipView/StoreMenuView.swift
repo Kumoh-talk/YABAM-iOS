ff3f6dd2f8bc4593a16139b644dc75ea32645b86
박효준 <gywns626@naver.com>
2025-05-07 00:58:22 +0900

    chore: 주석 추가

--- YABAM/Feature/Sources/Views/Home/Store/Detail/ChipView/StoreMenuView.swift
+++ YABAM/Feature/Sources/Views/Home/Store/Detail/ChipView/StoreMenuView.swift
@@ -22,6 +22,8 @@ final class DummyCartManager: CartManager {
         super.init()
     }
 
+    /// 스토어 메뉴 보기에서는 실제 카트 기능을 사용하지 않으므로
+    /// 카트 조작 메서드를 빈 구현으로 오버라이드합니다.
     override func add(_ item: MenuItem) { }
     override func merge(_ newItems: [MenuItem]) { }
     override func remove(_ item: MenuItem) { }
