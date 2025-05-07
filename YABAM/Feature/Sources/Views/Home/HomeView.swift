ab16ee2531520682bea66036e79968d727dd686b
박효준 <gywns626@naver.com>
2025-05-07 18:56:10 +0900

    feat: 각 탭바에 네비게이션 헤더 설정

--- YABAM/Feature/Sources/Views/Home/HomeView.swift
+++ YABAM/Feature/Sources/Views/Home/HomeView.swift
@@ -8,7 +8,7 @@ struct HomeView: View {
         .withNavigationButtons(
             leading: NavigationButtonConfig(content: {
                 Image(.yabamEmptyLogo).resizable().frame(width: 24, height: 24)
-                YBText("내 주변 주막", fontType: .mediumHeader4, color: .Neutral.neutral900)
+                YBText("내 주변 주막", fontType: .mediumHeader5, color: .Neutral.neutral900)
             }, action: {
                 
             })
--- YABAM/Feature/Sources/Views/OrderHistory/OrderHistoryView.swift
+++ YABAM/Feature/Sources/Views/OrderHistory/OrderHistoryView.swift
@@ -11,5 +11,13 @@ struct OrderHistoryView: View {
         }
         .background(Color.blue.opacity(0.1))
         .edgesIgnoringSafeArea(.bottom)
+        .withNavigationButtons(
+            leading: NavigationButtonConfig(content: {
+                Image(.yabamEmptyLogo).resizable().frame(width: 24, height: 24)
+                YBText("주문내역", fontType: .mediumHeader5, color: .Neutral.neutral900)
+            }, action: {
+                
+            })
+        )
     }
 }
