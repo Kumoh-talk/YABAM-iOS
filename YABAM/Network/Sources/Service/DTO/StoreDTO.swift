fa325453653665a896b4c6976a44b7f2c029050b
박효준 <gywns626@naver.com>
2025-05-10 00:12:54 +0900

    chore: 접근 제어자 수정 및 사용하지 않는 코드제거

--- YABAM/Network/Sources/Service/DTO/StoreDTO.swift
+++ YABAM/Network/Sources/Service/DTO/StoreDTO.swift
@@ -11,4 +11,32 @@ public struct StoreDTO: Decodable, Sendable {
     public let university: String
     public let tableTime: Int
     public let tableCost: Int
+    
+    public init(
+        storeId: Int,
+        isOpen: Bool,
+        userId: Int,
+        userNickname: String,
+        storeName: String,
+        latitude: Double,
+        longitude: Double,
+        description: String,
+        headImageUrl: String,
+        university: String,
+        tableTime: Int,
+        tableCost: Int
+    ) {
+        self.storeId = storeId
+        self.isOpen = isOpen
+        self.userId = userId
+        self.userNickname = userNickname
+        self.storeName = storeName
+        self.latitude = latitude
+        self.longitude = longitude
+        self.description = description
+        self.headImageUrl = headImageUrl
+        self.university = university
+        self.tableTime = tableTime
+        self.tableCost = tableCost
+    }
 }
