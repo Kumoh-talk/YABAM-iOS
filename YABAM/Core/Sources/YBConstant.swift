f89038c9b31446abd47282f6d86768b1c1d33953
Kyxxn <gywns626@naver.com>
2025-05-14 14:22:09 +0900

    feat: Gateway URL Configure 등록

--- YABAM/Core/Sources/YBConstant.swift
+++ YABAM/Core/Sources/YBConstant.swift
@@ -2,8 +2,9 @@ import Foundation
 
 public enum YBConstant {
     public static let baseURL = Bundle.main.infoDictionary?["BASE_SERVER_URL"] as? String ?? "http://localhost:8080"
-    public static let authPath = Bundle.main.infoDictionary?["AUTH_PATH"] as? String ?? "/pathError"
-    public static let userURL = Bundle.main.infoDictionary?["USER_PATH"] as? String ?? "/pathError"
+    public static let authPath = Bundle.main.infoDictionary?["AUTH_PATH"] as? String ?? "/authPathError"
+    public static let userURL = Bundle.main.infoDictionary?["USER_PATH"] as? String ?? "/userPathError"
+    public static let gatewayURL = Bundle.main.infoDictionary?["GATEWAY_PATH"] as? String ?? "/gatewayPathError"
     public static let accessToken = "accessToken"
     public static let refreshToken = "refreshToken"
     public static let refreshTokenExpiredAt = "refreshTokenExpiredAt"
